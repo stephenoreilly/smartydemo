b49f61105d66c72f62198e3321658ab19ee4ca03
stevo11850 <stiofan.oreilly@gmail.com>
2015-08-22 08:51:31 +0000

    adding db

--- connection.php
+++ connection.php
@@ -7,6 +7,46 @@
     
     $link = new mysqli($server, $username, $password, $db);
 
+ $query="CREATE TABLE users3 (
+        id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30) NOT NULL,
+        email VARCHAR(50),
+        password VARCHAR(50),
+        reg_date TIMESTAMP
+            );
+
+
+CREATE TABLE items (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2),
+    description VARCHAR(50),
+    reg_date TIMESTAMP
+    );
+CREATE TABLE shoppingcart (
+    
+    item_id INT(6) UNSIGNED NOT NULL,
+    user_id INT(6) UNSIGNED NOT NULL,
+    amount INT(6) UNSIGNED,
+    PRIMARY KEY (item_id, user_id)
+
+);
+ALTER TABLE shoppingcart
+    ADD FOREIGN KEY (user_id)
+    REFERENCES users3(id);
+    ALTER TABLE shoppingcart3
+    ADD FOREIGN KEY (item_id)
+    REFERENCES items(id);";
+
+
+
+    $res =mysqli_query($link,$query);
+    if(!$res){
+        echo "error";
+    }
+    else{
+        echo "no error";
+    }
      session_start();
      $sessionid=$_SESSION['id'];
 ?>
