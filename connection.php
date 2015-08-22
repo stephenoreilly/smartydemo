d581a211a3aeb06c30281756607aea2594357632
stevo11850 <stiofan.oreilly@gmail.com>
2015-08-22 08:53:10 +0000

    adding db

--- connection.php
+++ connection.php
@@ -13,30 +13,7 @@
         email VARCHAR(50),
         password VARCHAR(50),
         reg_date TIMESTAMP
-            );
-
-
-CREATE TABLE items (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
-    price DECIMAL(10,2),
-    description VARCHAR(50),
-    reg_date TIMESTAMP
-    );
-CREATE TABLE shoppingcart (
-    
-    item_id INT(6) UNSIGNED NOT NULL,
-    user_id INT(6) UNSIGNED NOT NULL,
-    amount INT(6) UNSIGNED,
-    PRIMARY KEY (item_id, user_id)
-
-);
-ALTER TABLE shoppingcart
-    ADD FOREIGN KEY (user_id)
-    REFERENCES users3(id);
-    ALTER TABLE shoppingcart3
-    ADD FOREIGN KEY (item_id)
-    REFERENCES items(id);";
+            );";
 
 
 
