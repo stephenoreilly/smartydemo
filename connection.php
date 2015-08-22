adfd5438e3645df57a38e1afd41aca840dcf8263
stevo11850 <stiofan.oreilly@gmail.com>
2015-08-22 09:02:31 +0000

    adding db

--- connection.php
+++ connection.php
@@ -7,24 +7,6 @@
     
     $link = new mysqli($server, $username, $password, $db);
 
- $query="
-ALTER TABLE shoppingcart
-    ADD FOREIGN KEY (user_id)
-    REFERENCES users3(id);
-    ALTER TABLE shoppingcart
-    ADD FOREIGN KEY (item_id)
-    REFERENCES items(id);";
-
-
-
-
-    $res =mysqli_query($link,$query);
-    if(!$res){
-        echo "error";
-    }
-    else{
-        echo "no error";
-    }
      session_start();
      $sessionid=$_SESSION['id'];
 ?>
